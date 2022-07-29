6617933914a02452389801db1692952709919357
FacundoVillarroel <facu.villarroel96@gmail.com>
2022-07-29 20:40:38 +0200

    First commit (Structure)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Navbar from "./components/navbar/Navbar"
+import Landing from "./components/landing/Landing"
+import AboutMe from "./components/aboutMe/AboutMe"
+import Projects from "./components/projects/Projects"
+import Tecnologies from "./components/tecnologies/Tecnologies"
+import Contact from "./components/contact/Contact"
+import Footer from "./components/footer/Footer"
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <Navbar/>
+      <Landing/>
+      <AboutMe/>
+      <Projects/>
+      <Tecnologies/>
+      <Contact/>
+      <Footer/>
     </div>
   );
 }
