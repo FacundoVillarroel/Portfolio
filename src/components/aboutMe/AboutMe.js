97f6dbd69481878204a89b8d0aae056f6513abb0
FacundoVillarroel <facu.villarroel96@gmail.com>
2022-08-01 23:32:25 +0200

    About Me

--- src/components/aboutMe/AboutMe.js
+++ src/components/aboutMe/AboutMe.js
@@ -1,8 +1,17 @@
-import React from 'react'
+import React from 'react';
+import programmingImg from "../../img/programmingImg.jpg"
 
 const AboutMe = () => {
   return (
-    <div>AboutMe</div>
+    <div className='aboutMeContainer'>
+      <div className='imgProfileContainer'>
+        <img src={programmingImg} className="imgProfile"></img>
+      </div>
+      <div className='descriptionContainer'>
+        <h1> About Me </h1>
+        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate</p>
+      </div>
+    </div>
   )
 }
 
